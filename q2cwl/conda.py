742f551e59f08c479d0735fa582526f5759b08ff
Chris Keefe <39198770+ChrisKeefe@users.noreply.github.com>
2018-11-02 11:53:48 -0700

    STY: updates for flake8 compliance (#6)

--- q2cwl/conda.py
+++ q2cwl/conda.py
@@ -14,6 +14,7 @@ import io
 
 import q2cwl.template as template
 
+
 def conda_list():
     results = subprocess.run(['conda', 'list', '--json'],
                              stdout=subprocess.PIPE, check=True)
